bcb88af478cec1062c57b494048c33bd0eb5d5aa
fabianofrank <fabianofrank@protonmail.com>
2022-09-21 12:51:11 -0300

    Create API endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root 'greetings#index'
+  namespace :api do
+    get 'greetings', to: 'greetings#index', defaults: { format: 'json' }
+  end
 end
